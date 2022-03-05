285c655fddc29324fcff75ea63d30c685343fff7
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-05 12:04:03 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBookProgram
+    
+{
+    public class Address
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book program");
+            AddressProgram.AddingInfo();
+        }
+    }
+}
